c501513b601832b99a745cac4b9f42a24de88d2b
Itoromfon <itoromfon746@gmail.com>
2022-09-20 22:56:47 +0100

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <h1>This is the Footer Page</h1>
+        <div className="footer">
+            
+
+        </div>
     )
 }
