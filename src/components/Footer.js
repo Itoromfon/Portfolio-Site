900bbb1fead6dccf5c532e069e584c6dc58aa423
Itoromfon <itoromfon746@gmail.com>
2022-09-20 01:09:52 +0100

    commit

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-    <footer className="footer">
-        <p className="footer-text"><small>2021 Itoro development. All rights reserved.</small></p>
-    </footer>
-    )
-}
