6b205776bc35a0e7524141b6dead217607f37822
Itoromfon <itoromfon746@gmail.com>
2022-09-20 02:57:05 +0100

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,15 @@ import React from "react"
 
 export default function Main() {
     return (
-        <h1>Main content goes here</h1>
+        <div className="main">
+            <h1 className="main-h1">Fun facts about React</h1>
+            <ul className="main-text">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousand of entreprise apps, including mobile apps</li>               
+            </ul>
+        </div>
     )
 }
