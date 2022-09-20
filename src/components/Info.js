6ae958eed86addad36db236fc70b3a0eb75ccda3
Itoromfon <itoromfon746@gmail.com>
2022-09-20 15:59:45 +0100

    commit

---
+++ src/components/Info.js
@@ -0,0 +1,109 @@
+import React from "react"
+import infoImage from "../images/itoro.jpg"
+
+export default function Info() {
+    return (
+        <div className="info">
+            <img src={infoImage} className="info-img" alt="info-img" /> 
+            <h1 className="info-h1">Itoro Mfon Emmanuel</h1>
+            <h3 className="info-h3">Frontend Developer</h3>
+            <h5 className="info-h5">Itoromfon.website</h5>
+            <button className="info-email-btn">
+                <div>
+                    <img src="" />
+                    Email
+                </div>
+            </button>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Interests.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <h1>This is the Interests Page</h1>
+    )
+}
