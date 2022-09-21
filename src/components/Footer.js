0fa2bc7f4373271179fecd00b962dff751937ce2
Itoromfon <itoromfon746@gmail.com>
2022-09-21 11:32:50 +0100

    Added some changes in Footer.js and index.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from "react"
+import twitter from "../images/Twitter Icon.png"
+import facebook from "../images/Facebook Icon.png"
+import instagram from "../images/Instagram Icon.png"
+import github from "../images/GitHub Icon.png"
 
 export default function Footer() {
     return (
         <div className="footer">
-            
-
+            <img src={twitter} className="twitter" alt="twitter" />
+            <img src={facebook} className="facebook" alt="facebook" />
+            <img src={instagram} className="instagram" alt="instagram" />
+            <img src={github} className="github" alt="github" />
         </div>
     )
 }
