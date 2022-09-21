f84d07e1cc99b5d484afde525f4aa39e242be43d
Itoromfon <itoromfon746@gmail.com>
2022-09-21 15:24:54 +0100

    Added some changes in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ import github from "../images/GitHub Icon.png"
 export default function Footer() {
     return (
         <div className="footer">
-            <img src={twitter} className="twitter" alt="twitter" />
-            <img src={facebook} className="facebook" alt="facebook" />
-            <img src={instagram} className="instagram" alt="instagram" />
-            <img src={github} className="github" alt="github" />
+            <a href="https://mobile.twitter.com/praiztech" target="_blank" rel="noreferrer"><img src={twitter} className="twitter" alt="twitter" /></a>
+            <a href="https://m.facebook.com/praise.mfon.73?eav=AfbRlvWHn1qfEDaq26JlDXb2gHH6uk2oMrOGQvUM3NZ5fmR3oEZDb4B3k8EXDvUnaXQ&paipv=0" target="_blank" rel="noreferrer"><img src={facebook} className="facebook" alt="facebook" /></a>
+            <a href="https://www.instagram.com/praiztech/" target="_blank" rel="noreferrer"><img src={instagram} className="instagram" alt="instagram" /></a>
+            <a href="https://github.com/Itoromfon?tab=repositories" target="_blank" rel="noreferrer"><img src={github} className="github" alt="github" /></a>
         </div>
     )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -11,18 +11,18 @@ export default function Info() {
             <h3 className="info-h3">Frontend Developer</h3>
             <h5 className="info-h5">Itoromfon.website</h5>
             <div className="buttons">
-                <button className="info-email-btn">
+                <a href="https://www.gmail.com" target="_blank" rel="noreferrer" className="info-email-btn">
                     <div className="info-image-text">
                         <img src={email} className="email-icon" alt="email-icon" />
                         <p className="info-text-email">Email</p>
                     </div>
-                </button>
-                <button className="info-link-btn">
+                </a>
+                <a href="https://www.linkedin.com/in/itoro-mfon-emmanuel-3867991aa/" target="_blank" rel="noreferrer" className="info-link-btn">
                     <div className="info-image-text">
                         <img src={linkedin} className="linkedin-icon" alt="linkedin-icon" />
                         <p className="info-text-linkedin">LinkedIn</p>
                     </div>
-                </button>
+                </a>
             </div>
         </div>
     )
