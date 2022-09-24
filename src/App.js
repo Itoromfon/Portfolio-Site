338ab182c545371905f6fa092a8ab0c4d4ad5677
Itoromfon <itoromfon746@gmail.com>
2022-09-24 17:07:46 +0100

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Joke from "./components/Joke"
 // import Navbar from "./components/Navbar"
 // import Hero from "./components/Hero"
 // import Card from "./components/Card"
