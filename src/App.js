10eef53f65a21e2b52ba224937eac8d4c975736b
Itoromfon <itoromfon746@gmail.com>
2022-09-28 23:01:50 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
-import data from "./data"
 
-export default function App() {
-    const mainData = data.map(item => 
-         <Main 
-                {...item}
-        />
-    )
-    return (
-        <div>
-            <Navbar />
-            {mainData}
-        </div>
-    )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import Navbar from "./components/Navbar"
+// import Main from "./components/Main"
+// import data from "./data"
+
+// export default function App() {
+//     const mainData = data.map(item => 
+//          <Main 
+//                 {...item}
+//         />
+//     )
+//     return (
+//         <div>
+//             <Navbar />
+//             {mainData}
+//         </div>
+//     )
+// }
 
 
 
