f5e1ab1d097f4b5858d8677733e0fe9b1df8b2f5
Itoromfon <itoromfon746@gmail.com>
2022-10-05 01:52:33 +0100

    added some in my JS and CSS files

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,38 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+    console.log(memeImage)
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        // memesArray[randomNumber].url
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
+                <p></p>
                 <div className="inputs">
-                    <input className="input" type="text" />
-                    <input className="input" type="text" />
+                    <input
+                        className="input" 
+                        type="text" 
+                        placeholder="Top text"
+                    />
+                    <input 
+                        className="input" 
+                        type="text" 
+                        placeholder="Bottom text"
+                    />
                 </div>
-                <a className="meme-btn" href="https://technology-firm.netlify.app/#" target="_blank" rel="noreferrer">
+                <a onClick={getMemeImage} className="meme-btn" href="#">
                     Get a new meme image
                 </a>
-            </form>
+            </div>
+            <div className="img-div">
+                <img src={memeImage} className="meme--image" href="./#" />
+            </div>
         </main>
     )
 }
