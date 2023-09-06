f0f7152577da927c324f2a58940ee0a4223d1113
itoromfon <itoromfon746@gmail.com>
2023-09-06 20:47:58 +0100

    added new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar(props) {
                 />
             </li>
             <li>
-          <a target='_blank' rel='noreferrer' href='https://docs.google.com/document/d/1_pkkIsJ_VyVamSGQMuDkyPsRdoxRjQo1/edit?usp=sharing&ouid=115927388427309803588&rtpof=true&sd=true' className='dark:bg-teal-500 bg-rose-400 hover:bg-teal-600 text-white font-bold text-lg px-4 py-2 rounded-lg ml-3 md:ml-8 cursor-pointer'>Resume</a>
+          <a target='_blank' rel='noreferrer' href='https://drive.google.com/file/d/1D7sNm6dPHrCNUmr4TtWwzkPzoqc71MTt/view?usp=sharing' className='dark:bg-teal-500 bg-rose-400 hover:bg-teal-600 text-white font-bold text-lg px-4 py-2 rounded-lg ml-3 md:ml-8 cursor-pointer'>Resume</a>
             </li>
         </ul>
     </nav>
